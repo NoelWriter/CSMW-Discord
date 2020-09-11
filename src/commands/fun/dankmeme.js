e9b44862ffb5b5a8c9053fc2a6879dfb0bed248c
Noel Writer <schr0112@hz.nl>
2020-09-11 15:35:49 +0200

    ⛄ Added random dankmeme command, with thanks to Michael

---
+++ src/commands/fun/dankmeme.js
@@ -0,0 +1,31 @@
+const { MessageEmbed } = require("discord.js");
+const superagent = require('superagent');
+
+module.exports = {
+    name: "dankmeme",
+    description: "Shows random dank meme picture",
+    category: "fun",
+    async execute(client, message) {
+        await (async () => {
+            try {
+                const res = await superagent.get('https://www.reddit.com/r/dankmemes/random.json').then(res => {
+
+                    const resData = JSON.parse(res.text)[0].data.children[0].data;
+                    const resUrl = resData.url_overridden_by_dest;
+                    const resTitle = resData.title;
+
+                    const embed = new MessageEmbed()
+                        .setTimestamp()
+                        .setFooter(message.author.username)
+                        .setColor("DARK_PURPLE")
+                        .setTitle(resTitle)
+                        .setImage(resUrl);
+
+                    message.channel.send(embed);
+                });
+            } catch (err) {
+                console.error(err);
+            }
+        })();
+    }
+};
