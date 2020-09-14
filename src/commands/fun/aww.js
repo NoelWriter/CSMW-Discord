c0067ffcdbdd1ebf069a5df2bb7ace8d0c8963fd
Noel Writer <schr0112@hz.nl>
2020-09-14 12:44:15 +0200

    Added aww command thanks to Michael

--- src/commands/fun/aww.js
+++ src/commands/fun/aww.js
@@ -2,14 +2,14 @@ const { MessageEmbed } = require("discord.js");
 const superagent = require('superagent');
 
 module.exports = {
-    name: "dankmeme",
-    description: "Shows random dank meme picture",
+    name: "aww",
+    description: "Shows random cute picture",
     category: "fun",
-    aliases: ["meme"],
+    aliases: ["cute"],
     async execute(client, message) {
         await (async () => {
             try {
-                const res = await superagent.get('https://www.reddit.com/r/dankmemes/random.json').then(res => {
+                const res = await superagent.get('https://www.reddit.com/r/aww/random.json').then(res => {
 
                     const resData = JSON.parse(res.text)[0].data.children[0].data;
 
@@ -18,7 +18,7 @@ module.exports = {
 
                     const resUrl = resData.url_overridden_by_dest;
                     const resTitle = resData.title;
-
+                    
                     const embed = new MessageEmbed()
                         .setTimestamp()
                         .setFooter(message.author.username)
