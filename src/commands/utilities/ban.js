927e8bb1d24d34806be02e6c757c9889215d4346
Pepijn Hillemans <pepijnhillemans@gmail.com>
2020-09-16 20:06:35 +0200

    Removed brackets from if in if(isabletoban)

--- src/commands/utilities/ban.js
+++ src/commands/utilities/ban.js
@@ -11,9 +11,9 @@ module.exports = {
 
             if (isAbleToBan) {
                 let userToBan = message.mentions.members.first() || { bannable: false };
-                if (userToBan.bannable) {
+                if (userToBan.bannable)
                     userToBan.ban();
-                } else message.reply('>>> The specific user is not bannable');
+                else message.reply('>>> The specific user is not bannable');
             } else {
                 message.reply('>>> You do not have the required permissions to execude this command');
             }
